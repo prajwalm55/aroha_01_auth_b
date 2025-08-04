a746d86ca32fb38d4b833c2ce7c0471ce084ec12
Prajwal M <prajwalm377@gmail.com>
2025-08-04 16:38:09 +0530

    allmost

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,3 @@
-
-
-// import mongoose from "mongoose";
-// import dotenv from 'dotenv';
-// // dotenv.config();
-// dotenv.config({ path: './.env' });
-
-
-// const connectDB = async () => {
-//   try {
-//     console.log("MONGO_URI from .env:", process.env.MONGO_URI); // 👈 Add this line
-
-//     // await mongoose.connect(process.env.MONGO_URI);
-//     await mongoose.connect("mongodb://localhost:27017/authDB");
-
-//     console.log("Mongoose Connected!!!");
-//   } catch (error) {
-//     console.log("Connection Failed", error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-// ✅ src/config/db.js
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config(); // load from .env
